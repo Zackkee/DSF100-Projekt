82b1d54e35083d7304185154e570752861172a72
Zacke <zbehrendtz@gmail.com>
2026-01-20 17:17:53 +0100

    gjorde ändringar i fastapi_server.py och skapade en fil som heter script,js fil för frontend

--- api/fastapi_server.py
+++ api/fastapi_server.py
@@ -1,4 +1,9 @@
+#uvicorn fastapi_server:app
+# rWYRx4lEoHYbc5sA databas lösenord
+
 from fastapi import FastAPI
 
 app = FastAPI()
-@app.get("/")
+@app.get("/") # Define a GET endpoint at the root URL
+def root():
+    return {"Det fungerar"} # Return a simple JSON response indicating that the server is working
